f7d485f19dd36711b75e73a6bc7eb3d71801973f
Paul Kim <khyunwoo1@gmail.com>
2021-02-21 21:21:36 -0500

    created cb func for fixing paths

--- index.js
+++ index.js
@@ -1,39 +1,15 @@
 const cp = require('child_process');
 const { stdout, stderr } = require('process');
 
+function pathResolver (foundPath) {
 
-// cp.exec('mkdir YOYOYO', {cwd: ".."}, (err, stdout, stderr) => {
-//   console.log('stdout', stdout)
-// });
-
-try {
-  // USER SHOULD BE ON DESKTOP DIRECTORY
-
-  const JMXInstallerWindows = 'curl --output JMXFILE.jar https://repo1.maven.org/maven2/io/prometheus/jmx/jmx_prometheus_javaagent/0.13.0/jmx_prometheus_javaagent-0.13.0.jar'
-  // const JMXInstallerLinux = 'sudo wget https://repo1.maven.org/maven2/io/prometheus/jmx/jmx_prometheus_javaagent/0.13.0/jmx_prometheus_javaagent-0.13.0.jar'
-
-  const currentWorkDir = {cwd: '..'}
-  cp.execSync(`mkdir JMXFOLDER`, currentWorkDir)
-  currentWorkDir.cwd = '../JMXFOLDER'
-
-  cp.execSync(JMXInstallerWindows, currentWorkDir)
-  console.log('JMX Exporter jar file successful')
-
-  // Find JMX FOLDER PATH
-  // MAKE THIS DYNAMIC!!!!
-  // the one that works: 'find /c/Users -type d -iname "Kafka-Server"'
-  console.log("...searching for Kafka-Server file Path...")
-  const kafkaServerDir = cp.execSync('find /c/Users/ching/Desktop -type d -iname "Kafka-Server"');
-  console.log('Kafka-Server directory found, copying JMX Exporter file to /Kafka-Server/libs/')
-
-
-  const kafkaServerStr = kafkaServerDir.toString();
   let newStr = '';
+
   // loop and end if el is "\"
   let starting = false;
-  for(let i = kafkaServerStr.length; i >=0; i--){
+  for(let i = foundPath.length; i >=0; i--){
     
-    let currentChar = kafkaServerStr[i];
+    let currentChar = foundPath[i];
     if(!starting){
       if(currentChar === 'r'){
         starting = true;
@@ -44,25 +20,37 @@ try {
     }
     newStr += currentChar
   }
-  let kafkaLibsDir = newStr.split('').reverse().join('') + '/libs/';
+  let fixedDir = newStr.split('').reverse().join('');
 
-  // Copy JMXFILE.jar into the Kafka-Server/libs/ 
-  // CLEARLY THIS WORKS: cp JMXFILE.jar /c/Users/ching/Desktop/Kafka-Server/libs/
+  return fixedDir
+}
+
+try {
+  // FIND USERS DESKTOP
+
+  const JMXInstallerWindows = 'curl --output JMXFILE.jar https://repo1.maven.org/maven2/io/prometheus/jmx/jmx_prometheus_javaagent/0.13.0/jmx_prometheus_javaagent-0.13.0.jar'
+  const JMXInstallerLinux = 'sudo wget https://repo1.maven.org/maven2/io/prometheus/jmx/jmx_prometheus_javaagent/0.13.0/jmx_prometheus_javaagent-0.13.0.jar'
+
+  const currentWorkDir = {cwd: '..'}
+  cp.execSync(`mkdir JMXFOLDER`, currentWorkDir)
+  currentWorkDir.cwd = '../JMXFOLDER'
+
+  cp.execSync(JMXInstallerWindows, currentWorkDir)
+  console.log('JMX Exporter jar file successful')
+
+  // Find JMX FOLDER PATH
+  // MAKE THIS DYNAMIC!!!!
+  console.log("...searching for Kafka-Server file Path...")
+  const kafkaServerDir = cp.execSync('find /c/Users/ching/Desktop -type d -iname "Kafka-Server"');
+  console.log('Kafka-Server directory found, copying JMX Exporter file to /Kafka-Server/libs/')
+
+  const kafkaServerStr = kafkaServerDir.toString();
+  const returned = pathResolver(kafkaServerStr);
+  let kafkaLibsDir = `${returned}/libs/`;
+ 
   cp.execSync('cp JMXFILE.jar ' + kafkaLibsDir, currentWorkDir);
+  console.log('exporter successfully copied to /kafka-server/libs/')
 
-  /**
-   *  1. Find out Kafka's Lib dir
-   *    - find .   (searches current director)    
-   *    - find <can be dir name>
-   *    - find . -type d  (finds all dirs and no files)
-   *    - find . -type f  (finds all files and no dirs)
-   * 
-   *    - find . -type f -name "<file name>"  finds file of exact name
-   *    - find . -type f -name "<file name>*" wild card for any fitting that description
-   *    - find . -type f -iname "<file name>" finds any files with name but not case sensitive
-   * 
-   *  2. Copy file onto Kafka's Lib dir
-   */
 
 } catch (err) {
   console.log("err", err);
