ebfe29aa4e7d89255329beb396795b1853ee622b
Paul Kim <khyunwoo1@gmail.com>
2021-02-24 18:36:22 -0500

    cleaned up

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const cp = require('child_process');
-const { start } = require('repl');
-
 
 try {
   // INITIAL SETUP
@@ -12,16 +10,12 @@ try {
 
   // Create directory and download JMX Exporter Agent
   const currentWorkDir = { cwd: '..' };
-  // cp.execSync(`mkdir JMXFOLDER`, currentWorkDir);
+  cp.execSync(`mkdir JMXFOLDER`, currentWorkDir);
   currentWorkDir.cwd = '../JMXFOLDER';
   cp.execSync(JMXInstallerWindows, currentWorkDir);
 
   // Find Kafka path and copy JMX Exporter file into libs dir
-  // const kafkaServerDir = cp.execSync(
-  //   'find /Users/shahprose/Desktop -type d -iname "kafka_2.13-2.7.0-test*"'
-  // );
   const kafkaServerDir = cp.execSync('find /home -type d -iname "kafka_2.13-2.7.0*"');
-  // const kafkaServerDir = cp.execSync('find /c/Users/ching/Desktop -type d -iname "kafka-server*"');
   const kafkaServerStr = kafkaServerDir.toString();
 
   const returned = kafkaServerStr.replace(/\n/g,'');
@@ -29,17 +23,12 @@ try {
   const kafkaLibsDir = `${returned}/libs/`;
   cp.execSync('cp JMXFILE.jar ' + kafkaLibsDir, currentWorkDir);
 
-  // CHECKS:
-  // Java path, kafka.service, kafka path
-
   // CONFIGURE EXPORTER
-  // Copy kafka-2_0_0.yml file onto config dir
   const kafkaConfigDir = `${returned}/config/`;
   const kafkaServerStart = `${returned}/bin/`;
   cp.execSync(`cp kafka-2_0_0.yml ${kafkaConfigDir}`);
 
   // Remove and replace existing kafka-server-start.sh file
-  // Check later to see if this can just be appended
   cp.execSync(`rm ${kafkaServerStart}kafka-server-start.sh`);
   cp.execSync(`cp kafka-server-start.sh ${kafkaServerStart}`);
   cp.execSync(`sudo chmod +x kafka-server-start.sh`);
@@ -61,7 +50,6 @@ try {
   cp.execSync(`echo "Type=simple" >> kafka.service`);
 
   let quotes = '"';
-  // Path to Java
   cp.execSync(
     `echo 'Environment=${quotes}JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64${quotes}' >> kafka.service`
   );
